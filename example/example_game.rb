026401f1745941232346282f7e183db902325be8
fussykyloren <mitchell.jones@eagles.oc.edu>
2020-05-13 18:47:50 -0500

    change gameplay setup

--- example/example_game.rb
+++ example/example_game.rb
@@ -6,4 +6,4 @@ puts "Welcome to Tic-Tac-Toe!"
 bob = TicTacToe::Player.new({color: "X", name: "bob"})
 frank = TicTacToe::Player.new({color: "O", name: "frank"})
 players = [bob, frank]
-TicTacToe::Game.new(players).play
+game = TicTacToe::Game.new(players).play
